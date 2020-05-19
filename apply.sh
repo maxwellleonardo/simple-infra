fcae6a595dbf1ec9872db1d7771ca62fd298692e
Maxwell Leonardo da Silva <maxwell.leonardo@gmail.com>
2020-05-19 12:31:51 -0300

    Fixing kafka and jenkins deployment

--- apply.sh
+++ apply.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
 
 build-jenkins() {
-  cd jenkins
-  docker build -t custom-jenkins .
-  cd ..
+    cd jenkins
+    docker build -t custom-jenkins .
+    cd ..
 }
 
 apply-terraform(){
-  terraform init
-  terraform apply -auto-approve
+    terraform init
+    terraform apply -auto-approve
 }
 
 kubectl-setup(){
-  terraform output kubeconfig > ~/.kube/config
-  terraform output config-map-aws-auth > /tmp/x.yml
-  aws eks --region us-east-1 update-kubeconfig --name simple-eks
-  kubectl apply -f /tmp/x.yml
-  rm /tmp/x.yml
+    cp ~/.kube/config /tmp/config-before
+    terraform output kubeconfig > ~/.kube/config
+    cp ~/.kube/config /tmp/config-terraform
+    aws eks --region us-east-1 update-kubeconfig --name simple-eks
+    cp ~/.kube/config /tmp/config-eks
+    terraform output config-map-aws-auth > /tmp/x.yml
+    kubectl apply -f /tmp/x.yml
+    rm /tmp/x.yml
 }
 
 start-jenkins(){
     aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 576962245852.dkr.ecr.us-east-1.amazonaws.com
-    docker build -t custom-jenkins .
     docker tag custom-jenkins:latest 576962245852.dkr.ecr.us-east-1.amazonaws.com/custom-jenkins:latest
     docker push 576962245852.dkr.ecr.us-east-1.amazonaws.com/custom-jenkins:latest
     
-    kubctl apply -f jenkins/jenkins.yml
+    kubectl apply -f jenkins/jenkins-deployment.yml
+    kubectl apply -f jenkins/jenkins-service.yml
+}
+
+kafka-setup(){
+    kubectl apply -f kafka/zookeeper.yml
+    kubectl apply -f kafka/kafka-broker1.yml
+    kubectl apply -f kafka/kafka-service.yml
+    #KAHN=$(kubectl describe svc kafka | grep "LoadBalancer Ingress" | awk -F ": " '{print $2}')
+    #echo "$KAHN"
+    #kubectl create configmap kafka-config --from-literal=kahn=
 }
 
 build-jenkins
 apply-terraform
 kubectl-setup
 start-jenkins
+kafka-setup
